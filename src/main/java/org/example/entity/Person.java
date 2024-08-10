aedcb5c75cdb7f1383ba1476d7ec1e07276ffc32
Alexey Rubnikovich <liftedfish@gmail.com>
2024-08-10 12:49:04 +0200

    pL4

--- src/main/java/org/example/entity/Person.java
+++ src/main/java/org/example/entity/Person.java
@@ -5,7 +5,13 @@ public class Person {
     private String dev1;
     private String dev2;
     private String dev3;
-    private String pL1;
-    private String pL2;
-    private String pL3;
+    private String dev4;
+    private String hotFix;
+    private String pR1;
+    private String pR2;
+    private String pR3;
+    private String g1;
+    private String g2;
+    private String g3;
+    private String g4;
 }
